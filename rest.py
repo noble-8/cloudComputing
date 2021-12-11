4dd8f82892998327c13b6a92b7068f4caf770cdb
ec2 <ubuntu@ip-172-31-29-123.us-east-2.compute.internal>
2021-12-11 00:14:41 +0000

    works

--- rest.py
+++ rest.py
@@ -1,6 +1,7 @@
 import json
 from flask import Flask
 from flask import request
+from flask import send_file
 import pymysql
 endpoint = 'wed8.c34yxqfbf9wx.us-east-2.rds.amazonaws.com'
 port = '3306'
@@ -25,8 +26,7 @@ app = Flask(__name__)
 table = 'cpop_users'
 @app.route('/')
 def home():
-	return 'hello world'
-
+	return send_file("figure.png", mimetype='image/gif')
 
 @app.route('/list')
 def list():
@@ -59,14 +59,19 @@ def get_by_name(name):
 def index():
 	#Id, name, age, gender, race = request.args['id'], request.args['name'], request.args['age'], request.args['gender'], request.args['race']
 	body = request.get_json( )
-	doc_id, email, password, name, age, gender, race = body['doctor'],body['email'],body['password'],body['name'],body['age'],body['gender'],body['race']
+	email, password, name, age, gender, race , doc_id , start_date , end_date = body['email'],body['password'],body['name'],body['age'],body['gender'],body['race'],body['doctor'] ,body['start_date'],body['end_date']
 	cnx = make_connection()
-	cursor = cnx.cursor()	
-	query = f'''insert into cpop_patients values (\"0\",\"{doc_id}\","{name}\",\"{age}\",\"{gender}\",\"{race}\")'''
-	cursor.execute(query)
-	cnx.commit()
-	cursor.close()
-	return "hello"
+	cursor = cnx.cursor();
+	#INSERT INTO cpop_patients VALUES (0,1,'2020-12-10 12:12:12','4712-12-12 00:00:00', 'Abhishek Verule', 25,'M', 1, 'abc@xyz.com');
+	query = f'''INSERT INTO cpop_patients VALUES (0,1,"{start_date}",'{end_date}', '{name}',{age} ,'{gender}',{race} , '{email}');''';
+	try:
+		cursor.execute(query)
+		cnx.commit()
+		cursor.close()
+		return "{msg:entered successfully}"
+	except  Exception as e: 
+		print(e);
+		return "check syntax"
 	
 
 
